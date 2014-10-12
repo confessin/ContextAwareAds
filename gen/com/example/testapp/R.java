43f458f36213f7963564ef85bf6b0c73e1bfa9c5
Abhishek Shukla <abhishek.shukla@inmobi.com>
2014-10-12 10:09:18 +0530

    adding dialog theme.

--- gen/com/example/testapp/R.java
+++ gen/com/example/testapp/R.java
@@ -36,6 +36,7 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int ad_activity=0x7f030001;
+        public static final int center_layour=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
